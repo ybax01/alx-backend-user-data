786aa683accf2451640ef8631ec722c256e65987
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-27 20:40:17 +0100

    1gg

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return - raises 401 error
     """
     abort(401)
-
