210055898aee12e1ce47e6cd8afea56cb0bf66fb
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-06-03 20:36:06 +0100

    eh

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
-from typing import TypeVar
 from user import Base, User
 
 
