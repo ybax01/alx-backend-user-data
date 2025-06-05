d0d3c4fea0092b1cceb683aaf81cc34ec1eb5aeb
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-06-05 15:08:41 +0100

    task12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,8 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
 
+from typing import Union
+
 
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
@@ -72,3 +74,17 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument
+        Returns a string or None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
