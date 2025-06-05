ad339f2c4ba2309d84b057bb798df627f304d55d
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-06-05 11:20:03 +0100

    try7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """API Routes for Authentication Service"""
 from auth import Auth
-from flask import (Flask, jsonify)
+from flask import (Flask, jsonify, request, abort)
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -11,6 +11,24 @@ AUTH = Auth()
 def hello_world() -> str:
     """ Base route for authentication service API """
     msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user if it does not exist before"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
     return jsonify(msg)
 
 
